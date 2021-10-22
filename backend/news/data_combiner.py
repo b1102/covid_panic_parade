f50d612abb7e7ae04128dd6b520a10f3ce03bad1
sergkobiakov <sergkobiakov@gmail.com>
2021-10-22 17:19:36 +0200

    feature: first run

--- backend/news/data_combiner.py
+++ backend/news/data_combiner.py
@@ -7,8 +7,8 @@ class ChartItem:
     country_name: str
     mass_media_chart_position: int
     covid_stats_chart_position: int
-    fractions_of_covid_news: float
     week_deaths_per_million: float
+    fractions_of_covid_news: float
     analyzed_news_entries_number: int
 
 
@@ -19,8 +19,8 @@ def combine_data(mass_media_impact_index, covid_stats) -> List[ChartItem]:
             country_name=country,
             mass_media_chart_position=list(mass_media_impact_index).index(country) + 1,
             covid_stats_chart_position=list(covid_stats).index(country) + 1,
-            fractions_of_covid_news=round(mass_media_impact_index[country][0] * 100, 1),
             week_deaths_per_million=covid_stats[country],
+            fractions_of_covid_news=round(mass_media_impact_index[country][0] * 100, 1),
             analyzed_news_entries_number=mass_media_impact_index[country][1]
         )
         toReturn.append(chartItem)
