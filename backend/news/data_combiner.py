4eb746aee52401c5f806c10254a4524e44b767a3
sergkobiakov <sergkobiakov@gmail.com>
2021-10-24 19:45:42 +0200

    feature: added tooltips

--- backend/news/data_combiner.py
+++ backend/news/data_combiner.py
@@ -4,11 +4,11 @@ from typing import List
 
 @dataclass
 class ChartItem:
-    country_name: str
     mass_media_chart_position: int
+    country_name: str
+    fractions_of_covid_news: float
     covid_stats_chart_position: int
     week_deaths_per_million: float
-    fractions_of_covid_news: float
     analyzed_news_entries_number: int
 
 
@@ -16,11 +16,11 @@ def combine_data(mass_media_impact_index, covid_stats) -> List[ChartItem]:
     toReturn = []
     for country in mass_media_impact_index:
         chartItem = ChartItem(
-            country_name=country,
             mass_media_chart_position=list(mass_media_impact_index).index(country) + 1,
+            country_name=country,
+            fractions_of_covid_news=round(mass_media_impact_index[country][0] * 100, 1),
             covid_stats_chart_position=list(covid_stats).index(country) + 1,
             week_deaths_per_million=covid_stats[country],
-            fractions_of_covid_news=round(mass_media_impact_index[country][0] * 100, 1),
             analyzed_news_entries_number=mass_media_impact_index[country][1]
         )
         toReturn.append(chartItem)
